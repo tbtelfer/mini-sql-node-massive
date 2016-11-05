f41ac7021138f89d6eddb3a09cfc1abb99fd43ee
Tom Telfer <tom@Toms-MacBook-Pro.local>
2016-11-05 13:35:42 -0600

    finished sql mini project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var massive = require('massive');
+var connectionString = "postgres://postgres:newday@localhost/sandbox";
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
@@ -6,7 +8,27 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
+// Use our connection string to get a copy/instance of massive to use.
+// Then add it to our app as a variable called db.
+var massiveInstance = massive.connectSync({connectionString : connectionString})
+app.set('db',massiveInstance);
 
+
+// require, then call the controller to get the planes in the database
+var controller = require('./controller.js');
+
+controller.getPlanes();
+///////THE CODE BELOW WAS MOVED TO controller.js
+// var db = app.get('db');
+// //add a new plane
+// db.new_plane(function(err, planes){
+//   console.log(err, "plane added")
+// })
+// // add planes from get_planes file
+// db.get_planes(function(err, planes){
+//     console.log(err, planes)
+// })
+
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000")
+})
